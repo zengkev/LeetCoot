fdd7a1946e51592e4b1f5fee72719e06d249f369
jzeng99 <jzeng99@gatech.edu>
2025-09-20 14:59:17 -0400

    back and reverse is the same

---
+++ 2Sum.py
@@ -0,0 +1,52 @@
+'''
+
+Code
+
+
+Testcase
+Testcase
+Test Result
+1. Two Sum
+Easy
+Topics
+premium lock icon
+Companies
+Hint
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+'''
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        # create hash map to store numbers we've seen and their indices
+
+        seenNumber = {}
+        # enumerate the list 
+        for currentIndex, num in enumerate(nums):
+            complement = target - num
+        # check if complement is in seenNumber
+            if complement in seenNumber:
+                return [seenNumber[complement], currentIndex]
+            seenNumber[num] = currentIndex
+        

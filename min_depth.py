ba08c1db42964439227a56f8691574499583cfdf
Kevin Zeng <zeng.kev@gmail.com>
2026-03-11 11:35:41 -0400

    added min_depth

---
+++ min_depth.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        
+
+        from collections import deque
+        
+        q = deque([root])
+        depth = 1
+
+        while node:
+            # what node is on this section
+            section = len(q)
+            for _ in range(section):
+                node = q.popleft()
+
+                if not node.left and not node.right:
+                    return depth
+                
+                if node.left:
+                    q.append(node.left)
+
+                if node.right:
+                    q.append(node.right)
+            
+            depth += 1
+
+        return depth
